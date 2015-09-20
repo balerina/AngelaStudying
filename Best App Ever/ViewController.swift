874bd6f027d80f090c0b8e02773005f2fe35b68b
Angelika Breitenbach <Angelika@MacBook-Air-Angelika.local>
2015-09-20 22:10:36 +0200

    Initial Commit

---
+++ Best App Ever/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Best App Ever
+//
+//  Created by Angelika Breitenbach on 20.09.2015.
+//  Copyright (c) 2015 Angelika Breitenbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
