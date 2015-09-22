b68456ce96744a6c6ebfb29c76c6c58561fac922
Angelika Breitenbach <Angelika@MacBook-Air-Angelika.local>
2015-09-22 11:58:14 +0200

    second commit

--- Best App Ever/SecondViewController.swift
+++ Best App Ever/SecondViewController.swift
@@ -10,26 +10,43 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var secretLabel: UILabel!
+    
+    
+    @IBOutlet weak var secretButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func buttonPressed(sender: UIButton) {
+        
+        if (secretLabel.hidden == true) {
+            secretLabel.hidden = false
+            secretLabel.textColor = UIColor.redColor()
+            
+            secretButton.backgroundColor = UIColor.purpleColor()
+            secretButton.setTitle("Sikret mesydż revealed!", forState: UIControlState.Normal)
 
-    /*
-    // MARK: - Navigation
+            
+        }
+        else {
+            // Adding comment to test commits
+            secretLabel.hidden = true
+            secretButton.backgroundColor = UIColor.clearColor()
+            secretButton.setTitle("Tap to see sikret mesydż!", forState: UIControlState.Normal)
+            
+            
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+            
+        }
+        
     }
-    */
 
 }
